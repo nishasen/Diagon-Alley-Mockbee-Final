7b211bfeb01d90848dc528bfd86df8bb1ada0c3e
nishasen <43262505+nishasen@users.noreply.github.com>
2022-03-29 00:33:43 +0530

    Update HeroImage.jsx

--- diagon-alley/src/Components/HeroImage/HeroImage.jsx
+++ diagon-alley/src/Components/HeroImage/HeroImage.jsx
@@ -6,13 +6,16 @@ import { useData } from '../../Context';
 import './HeroImage.css';
 
 function HeroImage() {
-  const { dispatch } = useData();
+  const { dispatch, setHouseFilter, setCategoryFilter } = useData();
   return (
     <div className="hero-image-container centered">
       <img src={DiagonAlley} alt="shop by house" className="hero-image"/>
       <h2 className="text-white hero-text">Your one stop store for your favourite Harry Potter theme based clothes.</h2>
       <Link to="/product-listing" className="btn-link">
-        <Button buttonText={"Shop now"} size={"large"} buttonBorder={true} onClick={()=>dispatch({type: "CLEAR_FILTERS"})}/>
+        <Button buttonText={"Shop now"} size={"large"} buttonBorder={true} onClick={()=>{
+          setCategoryFilter(true)
+          setHouseFilter(true)
+          dispatch({type: "CLEAR_FILTERS"})}}/>
       </Link>
     </div>
   )
